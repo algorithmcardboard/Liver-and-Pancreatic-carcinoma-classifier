87a84f238732965fda5e7a24bbc529c7ebf51747
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2015-12-19 16:38:48 -0500

    changes for getting the full file in a single step

--- src/preprocessing/convert.py
+++ src/preprocessing/convert.py
@@ -2,22 +2,36 @@ import csv
 from glob import glob
 import re
 
-FILES = glob("/scratch/ajr619/fml/methylation/liver/Level_3/*.sorted")
+FILES = glob("/scratch/ajr619/fml/*/Level_3/*.sorted")
 FILES.sort()
 
-OUT_FILE = '/scratch/ajr619/fml/methylation/liver/Level_3/data.csv'
-
-CANCER_TYPE = 2 
+OUT_FILE = '/scratch/ajr619/fml/output_file'
 
 output_writer = open(OUT_FILE, 'a')
+
+patients = dict()
+id = 0
 
 for file in FILES:
     print "processing file ", file
     output_writer.write("\n")
+
     m = re.search("TCGA-..-([A-Za-z0-9]{4,4})-(\d{2,2})[A-Za-z]", file)
     patient_id = m.group(1)
     tissue_type = m.group(2)
-    output_writer.write("{0} {1} {2} ".format(patient_id, CANCER_TYPE, tissue_type))
+
+    if patient_id not in patients:
+        id = id + 1
+        patients[patient_id] = id;
+
+    patient_id = patients[patient_id]
+
+    cancer_type = 2
+    tmp = re.search("PAAD", file)
+    if tmp is not None:
+        cancer_type = 1
+
+    output_writer.write("{0} {1} {2} ".format(patient_id, cancer_type, tissue_type))
     with open(file, 'r') as myFile:
         reader = csv.reader(myFile, delimiter='\t')
         for row in reader:
