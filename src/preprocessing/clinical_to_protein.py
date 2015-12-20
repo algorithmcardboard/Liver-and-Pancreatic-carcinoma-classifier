f5aa0c0ed6635c00e2b948be37ecce35e441f9c9
Anirudhan J Rajagopalan <ajr619@nyu.edu>
2015-12-20 00:51:12 -0500

    changing file name

--- src/preprocessing/clinical_to_protein.py
+++ src/preprocessing/clinical_to_protein.py
@@ -6,13 +6,14 @@ import re
 FILES = glob('/scratch/ajr619/fml/clinical/*/*.xml') 
 FILES.sort()
 
-PATIENT_FILES = glob("/scratch/ajr619/fml/*/Level_3/*.sorted")
+PATIENT_FILES = glob("/scratch/ajr619/fml/methylation/*/Level_3/*.sorted")
 PATIENT_FILES.sort()
+
+TMP_FILE = '/scratch/ajr619/fml/clinical/tmp.file.csv'
 
 patients = dict()
 id = 0
 for file in PATIENT_FILES:
-
     m = re.search("TCGA-..-([A-Za-z0-9]{4,4})-(\d{2,2})[A-Za-z]", file)
     patient_id = m.group(1)
 
@@ -20,11 +21,21 @@ for file in PATIENT_FILES:
         id = id + 1
         patients[patient_id] = id;
 
+print "length of patients dict is ", len(patients), len(PATIENT_FILES)
+
+writer = csv.writer(open(TMP_FILE, 'w'), delimiter=',')
+
 
 for file in FILES:
     tree = etree.parse(file)
     root = tree.getroot()
     nsmap = root.nsmap
+
+    cancer_type = 1
+    t = re.search('pancreas', file)
+    if t is None:
+        cancer_type = 2
+
     bio_specimen = root.xpath('//bio:bcr_shipment_portion_uuid', namespaces=nsmap)
     if len(bio_specimen) == 0:
         continue
@@ -35,11 +46,13 @@ for file in FILES:
     patient_text = None
     old_patient_names = None
 
+
     for t_p in tissues_and_patients:
+        out = list()
         tp = t_p.text
         m = re.search("TCGA-..-([A-Za-z0-9]{4,4})-(\d{2,2})[A-Za-z]", tp)
         patient_text = m.group(1)
-        tissue_type = m.group(2)
+        tissue_type = int(m.group(2))
 
         if old_patient_names is not None and  old_patient_names != patient_text:
             print "Patient name doesn't match", file, patient_text, old_patient_names
@@ -50,3 +63,6 @@ for file in FILES:
 
         old_patient_names = patient_text
         patient_id = patients[patient_text]
+
+        out.extend([filename, cancer_type, patient_id, tissue_type])
+        writer.writerow(out)
